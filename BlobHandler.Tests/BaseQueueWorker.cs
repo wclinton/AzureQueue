f5a0b1a02cf08ce0c08d6ef02b847520b12c6296
Wade Clinton <wade.clinton@sage.com>
2014-10-07 21:29:30 -0700

    Another commit

---
+++ BlobHandler.Tests/BaseQueueWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobHandler.Tests
+{
+    public abstract class BaseQueueWorker<T> :IWorker
+    {
+        public dynamic Execute(dynamic data)
+        {
+
+            var connectionData = data as ConnectionWorkflowWorkerData;
+
+
+
+
+
+
+
+        }
+
+        public abstract void Process (IEnumerable<T> data);
+    }
+}
---
+++ BlobHandler.Tests/ConnectionWorkflowWorkerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobHandler.Tests
+{
+    class ConnectionWorkflowWorkerData
+    {
+        public int TenantId { get; set; }
+
+        public List<Uri> BlobUris { get; set; }
+    }
+}
